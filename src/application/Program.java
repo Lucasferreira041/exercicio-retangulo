a91cf4cb2daa076ee1c9ee3d84a0e64d08bec980
Lucas Ferreira <lucasfsferreirasilva@gmail.com>
2026-09-03 19:34:00 -0300

    Exercício 1 - Retângulo

---
+++ src/application/Program.java
@@ -0,0 +1,28 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Rectangle;
+
+public class Program {
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		Rectangle rect = new Rectangle();			
+		System.out.println("Entre com a altura e a largura do Retangulo: ");
+		rect.altura = sc.nextDouble();
+		rect.largura = sc.nextDouble();
+		
+		System.out.printf("AREA = %.2f%n", rect.area());
+		System.out.printf("PERMITRO = %.2f%n", rect.perimetro());
+		System.out.printf("DIAGONAL = %.2f%n", rect.diagonal());
+		sc.close();
+		
+		
+	}
+
+}
